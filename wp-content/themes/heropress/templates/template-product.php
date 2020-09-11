7eef4625143bb1e16b2403c6c80a9864ee69a337
nagham <nagham.nader@lyth.de>
2020-09-11 10:31:32 +0200

    added empty produkt template/ simple db connection

---
+++ wp-content/themes/heropress/templates/template-product.php
@@ -0,0 +1,23 @@
+<?php
+/**
+Template Name: Produkt Template
+*/
+get_header();
+
+?>
+
+<section class="page-wrapper">
+	<div class="container">
+					
+		<div class="row padding-top-60 padding-bottom-60">	
+<?php 
+include __DIR__ . "/../db-connection.php";
+?>
+
+
+</div><!-- /.row -->
+	</div><!-- /.container -->
+</section>
+<?php
+get_footer();
+?>
