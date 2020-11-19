989403994bd884a3b658d3a49dd6d05c0551ad09
nagham <nagham.nader@lyth.de>
2020-11-19 19:39:50 +0100

    added product templste style css file && convert php file

--- wp-content/themes/mulke-theme/templates/template-product.php
+++ wp-content/themes/mulke-theme/templates/template-product.php
@@ -10,65 +10,23 @@ get_header();
 
 <section class="page-wrapper">
 <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
 <!-- To prevent forms from submitting again when refreshing the page // double pay, etc  -->
 <script>
     if ( window.history.replaceState ) {
         window.history.replaceState( null, null, window.location.href );
     }
 </script>
-<style>
-body {
-  overscroll-behavior-y: none;
-}
-.product-box-parent{
-    height: 100%;
-	border-radius: 10px;
-    background: linear-gradient(300deg, rgba(8,225,174,0.24273459383753504) 0%, rgba(152,222,91,0.40800070028011204) 31%, rgba(8,225,174,0.37718837535014005) 100%);
 
-}
-.product-box-parent-col{
-	position: relative;
+  <!-- include mulkeproduct Template css style  -->
 
-}
-p {
-    color: black;
-    font-size: 15px;
-    font-family: 'Raleway', sans-serif;
-}
-.product-row{
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/templates/product-template-style.css" />
 
-	/** same height for all columns */
-	display: -webkit-box;
-	display: -webkit-flex;
-	display: -ms-flexbox;
-	display: flex;
-	flex-wrap: wrap;
-} 
-.cart-btn-holder{
-    bottom: 2%;
-    position: absolute;
-}
-input[type=number]::-webkit-inner-spin-button {
-	/** show the number picker buttons all the time */
-  opacity: 1;
-}
-.product-img{
-	width:100% !important;
-    height: 250px!important;
-}
-.img-holder{
-	margin-top: 3%;
-    margin-bottom: 3%;
-}
-.prise-col{
-    margin-bottom: 25%;
-}
-</style>
 
 	<div class="container-fluid">
 					
         <?php 
-        
+        include "convert-product-info.php";
         include "mulke-db-connection.php";
         //show saved produkte
             $sql = "SELECT * FROM mulke_product";
@@ -110,52 +68,6 @@ input[type=number]::-webkit-inner-spin-button {
             echo "0 results";
             }
             $conn->close();
-           
-    function getProductLocation($key) {
-        $value = "";
-        switch($key){
-            case 1:
-                $value = "sonnig";
-            break; 
-            case 2:
-                $value = "vollsonnig";
-            break; 
-            case 3:
-                $value = "halbschattig";
-            break; 
-            case 4:
-                $value = "schattig";
-            break; 
-        }
-        return $value;
-    }
-    function getProductWaterConsumption($key) {
-        $value = "";
-        switch($key){
-            case 1:
-                $value = "mittel";
-            break; 
-            case 2:
-                $value = "mäßig feucht";
-            break; 
-            case 3:
-                $value = "feucht";
-            break; 
-            case 4:
-                $value = "leicht feucht";
-            break; 
-            case 5:
-                $value = "mäßig";
-            break; 
-            case 6:
-                $value = "mäßig trocken";
-            break; 
-            case 7:
-                $value = "trocken";
-            break; 
-        }
-        return $value;
-    }
     ?>
         <!-- check if the product side bar is created and show it with shopping cart   -->
     <?php if ( is_active_sidebar( 'sidebar-product' ) ) : ?>
