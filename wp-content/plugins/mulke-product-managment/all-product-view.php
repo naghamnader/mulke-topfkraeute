668da70799d12356f21baf726d59c06f87ba3dc2
nagham <nagham.nader@lyth.de>
2020-11-19 20:34:15 +0100

    changed edit product view to inline blocks

--- wp-content/plugins/mulke-product-managment/all-product-view.php
+++ wp-content/plugins/mulke-product-managment/all-product-view.php
@@ -95,103 +95,132 @@ if ($result->num_rows > 0) {
                 
               </div>
             </div>
+
+            <!--  Standort & Wasser Verbrauch Row -->
+            <div class='row'>
             
-            <!-- Produkt Standort Multiple Radios -->
-            <div class='form-group'>
-              <label class='col-md-4 control-label' for='product_location'>Produkt Standort</label>
-              <div class='col-md-4'> 
-                <label class='radio-inline' for='product_location-0'>
-                  <input type='radio' name='product_location' id='product_location-0' value='1'>
-                  sonnig
-                </label> 
-                <label class='radio-inline' for='product_location-1'>
-                  <input type='radio' name='product_location' id='product_location-1' value='2'>
-                  vollsonnig
-                </label> 
-                <label class='radio-inline' for='product_location-2'>
-                  <input type='radio' name='product_location' id='product_location-2' value='3'>
-                  halbschattig
-                </label> 
-                <label class='radio-inline' for='product_location-3'>
-                  <input type='radio' name='product_location' id='product_location-3' value='4'>
-                  schattig
-                </label>
+              <!-- Produkt Standort Multiple Radios -->
+              <div class='form-group col-md-5'>
+                <label class='col-md-4 control-label' for='product_location'>Produkt Standort</label>
+                <div class='col-md-10'> 
+                  <label class='radio-inline' for='product_location-0'>
+                    <input type='radio' name='product_location' id='product_location-0' value='1'>
+                    sonnig
+                  </label> 
+                  <label class='radio-inline' for='product_location-1'>
+                    <input type='radio' name='product_location' id='product_location-1' value='2'>
+                    vollsonnig
+                  </label> 
+                  <label class='radio-inline' for='product_location-2'>
+                    <input type='radio' name='product_location' id='product_location-2' value='3'>
+                    halbschattig
+                  </label> 
+                  <label class='radio-inline' for='product_location-3'>
+                    <input type='radio' name='product_location' id='product_location-3' value='4'>
+                    schattig
+                  </label>
+                </div>
               </div>
+              
+              <!-- Wasser Verbrauch Multiple Radios -->
+              <div class='form-group col-md-5'>
+                <label class='col-md-4 control-label' for='water_consumption'>Wasser Verbrauch</label>
+                <div class='col-md-10'> 
+                  <label class='radio-inline' for='water_consumption_0'>
+                    <input type='radio' name='water_consumption' id='water_consumption_mittel' value='1'>
+                    mittel
+                  </label> 
+                  <label class='radio-inline' for='water_consumption_1'>
+                    <input type='radio' name='water_consumption' id='water_consumption_maeßigFeucht' value='2'>
+                    mäßig feucht
+                  </label> 
+                  <label class='radio-inline' for='water_consumption_2'>
+                    <input type='radio' name='water_consumption' id='water_consumption_feucht' value='3'>
+                    feucht
+                  </label> 
+                  <label class='radio-inline' for='water_consumption_3'>
+                    <input type='radio' name='water_consumption' id='water_consumption_leichtFeucht' value='4'>
+                    leicht feucht
+                  </label> 
+                  <label class='radio-inline' for='water_consumption_4'>
+                    <input type='radio' name='water_consumption' id='water_consumption_maeßig' value='5'>
+                    mäßig
+                  </label> 
+                  <label class='radio-inline' for='water_consumption_5'>
+                    <input type='radio' name='water_consumption' id='water_consumption_maeßigTrocken' value='6'>
+                    mäßig trocken
+                  </label> 
+                  <label class='radio-inline' for='water_consumption_6'>
+                    <input type='radio' name='water_consumption' id='water_consumption_trocken' value='7'>
+                    trocken
+                  </label>
+                </div>
+              </div>
+            
+            </div>
+
+            <!--  Bild & Verwendung Row -->
+            <div class='row'>
+
+              <!-- Produkt Bild Url Text input-->
+              <div class='form-group col-md-5'>
+                  <label class='col-md-4 control-label' for='product_img'>Produkt Bild Url</label>  
+                <div class='col-md-10'>
+                  <input id='product_img' name='product_img' type='text' placeholder='Produkt Bild Url' class='form-control input-md' >
+                  
+                </div>
+              </div>
+          
+              <!-- Produkt Verwendung Text input-->
+              <div class='form-group col-md-5'>
+                <label class='col-md-4 control-label' for='product_usage'>Produkt Verwendung</label>  
+                <div class='col-md-10'>
+                <input id='product_usage' name='product_usage' type='text' placeholder='Produkt Verwendung' class='form-control input-md'>
+                  
+                </div>
+              </div>
+
             </div>
             
-            <!-- Wasser Verbrauch Multiple Radios -->
-            <div class='form-group'>
-              <label class='col-md-4 control-label' for='water_consumption'>Wasser Verbrauch</label>
-              <div class='col-md-4'> 
-                <label class='radio-inline' for='water_consumption_0'>
-                  <input type='radio' name='water_consumption' id='water_consumption_mittel' value='1'>
-                  mittel
-                </label> 
-                <label class='radio-inline' for='water_consumption_1'>
-                  <input type='radio' name='water_consumption' id='water_consumption_maeßigFeucht' value='2'>
-                  mäßig feucht
-                </label> 
-                <label class='radio-inline' for='water_consumption_2'>
-                  <input type='radio' name='water_consumption' id='water_consumption_feucht' value='3'>
-                  feucht
-                </label> 
-                <label class='radio-inline' for='water_consumption_3'>
-                  <input type='radio' name='water_consumption' id='water_consumption_leichtFeucht' value='4'>
-                  leicht feucht
-                </label> 
-                <label class='radio-inline' for='water_consumption_4'>
-                  <input type='radio' name='water_consumption' id='water_consumption_maeßig' value='5'>
-                  mäßig
-                </label> 
-                <label class='radio-inline' for='water_consumption_5'>
-                  <input type='radio' name='water_consumption' id='water_consumption_maeßigTrocken' value='6'>
-                  mäßig trocken
-                </label> 
-                <label class='radio-inline' for='water_consumption_6'>
-                  <input type='radio' name='water_consumption' id='water_consumption_trocken' value='7'>
-                  trocken
-                </label>
+            <!--  Preis & Menge  Row -->
+            <div class='row'>
+
+              <!-- Produkt Prise Text input-->
+              <div class='form-group col-md-5'>
+                <label class='col-md-4 control-label' for='product_price'>Produkt Prise</label>  
+                <div class='col-md-10'>
+                <!-- placeholder Übersetzen-->
+                <input id='product_price' name='product_price' type='text' placeholder='Bitte die Prise cents mit '.' angebeb' class='form-control input-md' onkeypress='return isNumberKey(event)' >
+                  
+                </div>
               </div>
-            </div>
-            
-            <!-- Produkt Bild Url Text input-->
-             <div class='form-group'>
-                <label class='col-md-4 control-label' for='product_img'>Produkt Bild Url</label>  
-              <div class='col-md-4'>
-                <input id='product_img' name='product_img' type='text' placeholder='Produkt Bild Url' class='form-control input-md' >
-                
+
+              <!-- Verfügbare Menge Text input-->
+              <div class='form-group col-md-5'>
+                <label class='col-md-4 control-label' for='available_quantity'>Verfügbare Menge</label>  
+                <div class='col-md-10'>
+                <input id='available_quantity' name='available_quantity' onkeypress='return isNumberKey(event)' placeholder='Verfügbare Menge' class='form-control input-md'> 
+                </div>
               </div>
+
             </div>
-        
-            <!-- Produkt Verwendung Text input-->
-            <div class='form-group'>
-              <label class='col-md-4 control-label' for='product_usage'>Produkt Verwendung</label>  
-              <div class='col-md-4'>
-              <input id='product_usage' name='product_usage' type='text' placeholder='Produkt Verwendung' class='form-control input-md'>
-                
+
+            <!--  Submit & Cancel  Row -->
+            <div class='row'>
+
+              <!-- Update Button -->
+
+              <div class='col-md-5 '>
+                <input class='btn btn-primary float-right' type='button'  type='submit' name = 'submit'  value='submit' id='updateproduct'>
               </div>
-            </div>
-            
-            <!-- Produkt Prise Text input-->
-            <div class='form-group'>
-              <label class='col-md-4 control-label' for='product_price'>Produkt Prise</label>  
-              <div class='col-md-4'>
-              <!-- placeholder Übersetzen-->
-              <input id='product_price' name='product_price' type='text' placeholder='Bitte die Prise cents mit '.' angebeb' class='form-control input-md' onkeypress='return isNumberKey(event)' >
-                
+
+              <!-- Cancel Button -->
+              <div class='col-md-5'>
+                <input class='btn btn-danger' type='button'  type='cancel' name = 'cancel'  value='cancel' id='cancelupdate'>
               </div>
+
             </div>
-             <!-- Verfügbare Menge Text input-->
-             <div class='form-group'>
-              <label class='col-md-4 control-label' for='available_quantity'>Verfügbare Menge</label>  
-              <div class='col-md-4'>
-              <input id='available_quantity' name='available_quantity' onkeypress='return isNumberKey(event)' placeholder='Verfügbare Menge' class='form-control input-md'>
-                
-              </div>
-            </div>
-            <!-- Submit Button -->
-            <input class='btn btn-primary' type='button'  type='submit' name = 'submit'  value='submit' id='saveproduct'>
-            
+
             </form>
             </td>
             </tr>";
