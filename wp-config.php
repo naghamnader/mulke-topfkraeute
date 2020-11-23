5dee369f0a76d182bbad362ee037550be069e70b
nagham <nagham.nader@lyth.de>
2020-11-23 12:44:23 +0100

    new DB / disable wp debug

--- wp-config.php
+++ wp-config.php
@@ -102,8 +102,7 @@ $table_prefix = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG', false );
 
 /* Das war’s, Schluss mit dem Bearbeiten! Viel Spaß. */
 /* That's all, stop editing! Happy publishing. */
